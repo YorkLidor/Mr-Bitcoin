9033b93488931bd727a49ec7bbd98644a1572881
Lidor York <yorklidor@gmail.com>
2023-02-14 18:32:45 +0200

    fix defineConfig

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Mr.-BitCoin/',
+  base: '/Mr-BitCoin/',
   plugins: [vue()],
   resolve: {
     alias: {
